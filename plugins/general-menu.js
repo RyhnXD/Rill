7c1881ca7a3212c2b145687c3141fb3774da6437
Kaguya Shinomiya <102699105+RyhnXD@users.noreply.github.com>
2022-07-13 18:44:35 +0700

    Update general-menu.js

--- plugins/general-menu.js
+++ plugins/general-menu.js
@@ -99,10 +99,10 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     //conn.sendHydrated(m.chat, text.trim(), conn.getName(conn.user.jid), await genProfile(conn, m), 'https://youtube.com/channel/UC0hs_I8N3JntK5vO6KogavQ', 'YouTube', null, null, [['Speedtest', _p + 'ping'], ['Owner', _p + 'owner']], m)
    // conn.sendMessage(m.chat, { video: { url: 'https://telegra.ph/file/c82d5c358495e8ef15916.mp4' }, gifPlayback: true, gifAttribution: ~~(Math.random() * 2), caption: text.trim(), footer: await conn.getName(conn.user.jid) , templateButtons: [{ quickReplyButton: { displayText: 'Speedtest', id: `${_p}ping` }}, { quickReplyButton: { displayText: 'Owner', id: `${_p}owner` }} ] })
    conn.sendButton(m.chat, `*${wish()}, ${name} 👋*`, text.trim(), await genProfile(conn, m), [['Runtime', _p + 'runtime'], ['Owner', _p + 'owner']], false, { quoted: fkon, contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://chat.whatsapp.com/L2tzOxPcnH78cHpaBDAwsS",
+    mediaUrl: "https://chat.whatsapp.com/E8QcShiFEyrGddlExcmVIE",
     mediaType: "FOTO",
     description: "https://Instagram.com/bot_whangsaf", 
-    title: '『×ºＡｎｉｍｅ オタクº×』',
+    title: 'Sakura Botz,
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
